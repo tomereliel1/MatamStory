c030ffb40db2c655d20055ffb0472c272cf17f52
tomereliel1 <tomereliel@Tomer-pc>
2024-08-07 12:18:39 +0300

    creating Character derived classes files

---
+++ Players/Responsible.h
@@ -0,0 +1,8 @@
+//
+// Created by tomer on 8/4/2024.
+//
+
+#ifndef HW4_RESPONSIBLE_H
+#define HW4_RESPONSIBLE_H
+
+#endif //HW4_RESPONSIBLE_H
---
+++ Players/RiskTaking.h
@@ -0,0 +1,8 @@
+//
+// Created by tomer on 8/4/2024.
+//
+
+#ifndef HW4_RISKTAKING_H
+#define HW4_RISKTAKING_H
+
+#endif //HW4_RISKTAKING_H
